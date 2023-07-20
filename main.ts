3ab62d0b080f65295397954fa83d193cd9b91b9c
Ron Kosova <ron.kosova@gmail.com>
2023-07-20 22:53:08 +0300

    Better regex

--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
-import { Editor, MarkdownView, Plugin } from 'obsidian';
+import { Editor, MarkdownView, Plugin, Notice } from 'obsidian';
 
 
 export default class MarkdownLinkSpaceEncoder extends Plugin {
 
 	async onload() {
 		
-		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'markdown-link-space-encode',
 			name: 'Encode spaces to %20',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				let mdLinkUnencoded: string = editor.getSelection();
-				// mdLinkUnencoded.replace(/(?!\[[a-zA-Z0-9]+\]\([a-zA-Z0-9]+\))/g, '')
-				// console.log(mdLinkUnencoded);
-				editor.replaceSelection(mdLinkUnencoded.replace(/ /g, "%20"));
+				let selection: string = editor.getSelection();
+				let split = selection.match(/(\[.*\])(\(.*\))/);
+				if (split) {
+					let link = split[2].replace(/ /g, '%20');
+					editor.replaceSelection(split[1] + link);
+				} else {
+					editor.replaceSelection(selection);
+					new Notice("No link in selection, or incorrect formatting!");
+				}
 			}
 		});
 
