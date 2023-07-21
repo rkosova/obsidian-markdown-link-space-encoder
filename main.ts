64a43facfc1d6a9b96d37c1a9ec38905c5f2c798
Ron Kosova <ron.kosova@gmail.com>
2023-07-21 14:12:59 +0300

    Removed debug logs

--- main.ts
+++ main.ts
@@ -12,7 +12,6 @@ export default class MarkdownLinkSpaceEncoder extends Plugin {
 				let selection: string = editor.getSelection();
 				let replacedSel: string = selection.replace(/\[[^\]]*\]\([^\)]*\)/g, (match: string): string => {
 					let split: RegExpMatchArray | null = match.match(/(\[.*\])(\(.*\))/);
-					console.log(split);
 					if (split) {
 						return split[1] + split[2].replace(/ /g, '%20');
 					}
